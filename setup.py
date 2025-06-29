dc2c3440e1d0b9faeee8978325ec8c2767540c5a
Joanna A. Krupka <32344189+ashakru@users.noreply.github.com>
2025-06-29 19:06:04 +0100

    Pip installation compatibile

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='RibORF',
+    version='2.0',
+    description='Identifying genome-wide translated open reading frames using ribosome profiling',
+    author='Zhe Ji',
+    author_email='zhe.ji@northwestern.edu',
+    url='https://github.com/zhejilab/RibORF/tree/master/RibORF.2.0',
+    license='GNU GPL v3',
+    # tells setuptools to install any scripts under bin/
+    scripts=['bin/'],
+    # no Python dependencies:
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Perl',
+        'License :: GNU GPL v3',
+        'Operating System :: OS Independent',
+    ],
+    zip_safe=False,
+)
